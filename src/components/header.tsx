7e56fb740704bec6a36363e32b80101921bb87fa
Sunil Solanki <sunilsolanki630s@gmail.com>
2025-07-10 06:19:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold font-headline">Solanki.Dev</span>
